d1a24cc9ebdd383c2b0fc65626c2b473864fbb2e
hufeng <15574874109@163.com>
2018-03-19 00:59:30 +0800

    home sumbit

--- xbin-store-common-pojo/src/main/java/cn/binux/pojo/TbOrderItem.java
+++ xbin-store-common-pojo/src/main/java/cn/binux/pojo/TbOrderItem.java
@@ -22,4 +22,75 @@ public class TbOrderItem {
 
     private String weights;
 
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getItemId() {
+        return itemId;
+    }
+
+    public void setItemId(String itemId) {
+        this.itemId = itemId;
+    }
+
+    public String getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(String orderId) {
+        this.orderId = orderId;
+    }
+
+    public Integer getNum() {
+        return num;
+    }
+
+    public void setNum(Integer num) {
+        this.num = num;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Long getPrice() {
+        return price;
+    }
+
+    public void setPrice(Long price) {
+        this.price = price;
+    }
+
+    public Long getTotalFee() {
+        return totalFee;
+    }
+
+    public void setTotalFee(Long totalFee) {
+        this.totalFee = totalFee;
+    }
+
+    public String getPicPath() {
+        return picPath;
+    }
+
+    public void setPicPath(String picPath) {
+        this.picPath = picPath;
+    }
+
+    public String getWeights() {
+        return weights;
+    }
+
+    public void setWeights(String weights) {
+        this.weights = weights;
+    }
 }
--- xbin-store-service-sso/src/main/java/cn/binux/XbinStoreServiceSSOApplication.java
+++ xbin-store-service-sso/src/main/java/cn/binux/XbinStoreServiceSSOApplication.java
@@ -1,11 +1,11 @@
 package cn.binux;
 
-import com.ctrip.framework.apollo.spring.annotation.EnableApolloConfig;
-import org.mybatis.spring.annotation.MapperScan;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
+		import com.ctrip.framework.apollo.spring.annotation.EnableApolloConfig;
+		import org.mybatis.spring.annotation.MapperScan;
+		import org.springframework.boot.SpringApplication;
+		import org.springframework.boot.autoconfigure.SpringBootApplication;
+		import org.springframework.context.annotation.Configuration;
+		import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
 @EnableApolloConfig
